8eff7b5e6b6dc6f70b857cae155f00413bfcab59
EdmarAugust0 <edmar.contato149@gmail.com>
2024-08-01 15:57:05 -0300

    :sparkles: feat: Contrução da página de checkout

---
+++ src/pages/Checkout/styles.ts
@@ -0,0 +1,25 @@
+import styled from 'styled-components'
+import { cores } from '../../styles'
+
+export const Row = styled.div`
+  display: flex;
+  column-gap: 24px;
+`
+
+export const InputGroup = styled.div`
+  flex: auto;
+
+  label {
+    display: block;
+    font-size: 14px;
+    margin-bottom: 8px;
+  }
+
+  input {
+    background-color: ${cores.branca};
+    border: 1px solid ${cores.branca};
+    height: 32px;
+    padding: 0 8px;
+    width: 100%;
+  }
+`
