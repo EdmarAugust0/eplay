065a9c188a978958a69b98be1da5977679a0dcc3
EdmarAugust0 <edmar.contato149@gmail.com>
2024-08-02 14:11:36 -0300

    :sparkles: feat: criando a area de foma de pagamento com a interação de tipos de pagamentos

--- src/pages/Checkout/styles.ts
+++ src/pages/Checkout/styles.ts
@@ -1,13 +1,29 @@
 import styled from 'styled-components'
 import { cores } from '../../styles'
 
-export const Row = styled.div`
+type InputGroupProps = {
+  maxWidth?: string
+}
+
+type RowProps = {
+  marginTop?: string
+}
+
+type TabButtonProps = {
+  isActive: boolean
+}
+
+export const Row = styled.div<RowProps>`
   display: flex;
   column-gap: 24px;
+  margin-top: ${(props) => props.marginTop || '0'};
+  align-items: flex-end;
 `
 
-export const InputGroup = styled.div`
+export const InputGroup = styled.div<InputGroupProps>`
   flex: auto;
+
+  max-width: ${(props) => props.maxWidth || 'auto'};
 
   label {
     display: block;
@@ -15,11 +31,27 @@ export const InputGroup = styled.div`
     margin-bottom: 8px;
   }
 
-  input {
+  input,
+  select {
     background-color: ${cores.branca};
     border: 1px solid ${cores.branca};
     height: 32px;
     padding: 0 8px;
     width: 100%;
+  }
+`
+
+export const TabButton = styled.button<TabButtonProps>`
+  border-radius: 8px;
+  font-size: 14px;
+  font-weight: bold;
+  color: ${cores.branca};
+  background-color: ${(props) => (props.isActive ? cores.verde : cores.preta)};
+  border: 1px solid ${cores.preta};
+  padding: 8px;
+  margin-right: 16px;
+
+  img {
+    margin-right: 8px;
   }
 `
